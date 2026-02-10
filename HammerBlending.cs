3d2284ecfb9d1e22608e103ef54630464e126eae
Shringe <dashingkoso@gmail.com>
2026-02-10 08:20:23 -0600

    basic functionality
    Basic functionality works, but some graphical bugs persist

--- HammerBlending.cs
+++ HammerBlending.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.Drawing;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace HammerBlending
@@ -18,17 +14,48 @@ namespace HammerBlending
 
   }
 
-  internal class HammerBlendingTile : GlobalTile
+  internal class TileBlending : GlobalTile
   {
     public override void DrawEffects(int i, int j, int type, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
     {
-      // This function is currently registering every tile as a special point
-      Main.instance.TilesRenderer.AddSpecialLegacyPoint(i, j);
+      Tile tile = Main.tile[i, j];
+
+      bool shouldRegister = tile.Slope switch
+      {
+        SlopeType.SlopeDownLeft => Main.tile[i + 1, j].IsHalfBlock,
+        SlopeType.SlopeDownRight => Main.tile[i - 1, j].IsHalfBlock,
+        _ => false
+      };
+
+      if (shouldRegister)
+        Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomSolid);
     }
 
     public override void SpecialDraw(int i, int j, int type, SpriteBatch spriteBatch)
     {
-      // This draws special effects on registered tiles
+      Tile tile = Main.tile[i, j];
+
+      Texture2D texture = Main.instance.TilesRenderer.GetTileDrawTexture(tile, i, j);
+      Vector2 position = new Vector2(i * 16, j * 16) - Main.screenPosition;
+
+      // Get the bottom half of the full block texture
+      // I'm not sure if this is the correct way to draw a halfBlock
+      Rectangle halfBlockSource = new Rectangle(
+        tile.TileFrameX,
+        tile.TileFrameY + 8,
+        16,
+        8
+      );
+
+      // Adjust destination to draw bottom half only
+      Rectangle destination = new Rectangle(
+        (int)position.X,
+        (int)position.Y + 8,
+        16,
+        8
+      );
+
+      spriteBatch.Draw(texture, destination, halfBlockSource, Lighting.GetColor(i, j));
     }
   }
 }
