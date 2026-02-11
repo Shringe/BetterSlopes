51634f1b192c567e3b19e75f1980915fc204e7c6
Shringe <dashingkoso@gmail.com>
2026-02-11 03:53:04 -0600

    refactor to SpecialTile class for higher-level drawing

--- HammerBlending.cs
+++ HammerBlending.cs
@@ -14,9 +14,54 @@ namespace HammerBlending
 
   }
 
+  // Used for drawing the custom blended tile sprites
+  internal class SpecialTile
+  {
+    private readonly Tile _tile;
+    private readonly Vector2 _position;
+    private readonly Texture2D _texture;
+    private readonly Color _lightingColor;
+    private readonly SpriteBatch _spriteBatch;
+
+    public SpecialTile(int i, int j, SpriteBatch spriteBatch)
+    {
+      Tile tile = Main.tile[i, j];
+      Vector2 position = new Vector2(i * 16, j * 16) - Main.screenPosition;
+      Texture2D texture = Main.instance.TilesRenderer.GetTileDrawTexture(tile, i, j);
+      Color lightingColor = Lighting.GetColor(i, j);
+
+      _tile = tile;
+      _position = position;
+      _texture = texture;
+      _lightingColor = lightingColor;
+      _spriteBatch = spriteBatch;
+    }
+
+    public void DrawHalfBlock()
+    {
+      // Get the bottom half of the full block texture
+      Rectangle halfBlockSource = new Rectangle(
+        _tile.TileFrameX,
+        _tile.TileFrameY + 8,
+        16,
+        8
+      );
+
+      // Adjust destination to draw bottom half only
+      Rectangle destination = new Rectangle(
+        (int)_position.X,
+        (int)_position.Y + 8,
+        16,
+        8
+      );
+
+      _spriteBatch.Draw(_texture, destination, halfBlockSource, _lightingColor);
+    }
+  }
+
   internal class TileBlending : GlobalTile
   {
-    public override void DrawEffects(int i, int j, int type, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+    public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch)
     {
       Tile tile = Main.tile[i, j];
 
@@ -29,33 +74,14 @@ namespace HammerBlending
 
       if (shouldRegister)
         Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomSolid);
+
+      return !shouldRegister;
     }
 
     public override void SpecialDraw(int i, int j, int type, SpriteBatch spriteBatch)
     {
-      Tile tile = Main.tile[i, j];
-
-      Texture2D texture = Main.instance.TilesRenderer.GetTileDrawTexture(tile, i, j);
-      Vector2 position = new Vector2(i * 16, j * 16) - Main.screenPosition;
-
-      // Get the bottom half of the full block texture
-      // I'm not sure if this is the correct way to draw a halfBlock
-      Rectangle halfBlockSource = new Rectangle(
-        tile.TileFrameX,
-        tile.TileFrameY + 8,
-        16,
-        8
-      );
-
-      // Adjust destination to draw bottom half only
-      Rectangle destination = new Rectangle(
-        (int)position.X,
-        (int)position.Y + 8,
-        16,
-        8
-      );
-
-      spriteBatch.Draw(texture, destination, halfBlockSource, Lighting.GetColor(i, j));
+      SpecialTile specialTile = new(i, j, spriteBatch);
+      specialTile.DrawHalfBlock();
     }
   }
 }
