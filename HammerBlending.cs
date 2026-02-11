307b5d72dce52f8e6111daaa8a173106c604e28c
Shringe <dashingkoso@gmail.com>
2026-02-11 04:29:15 -0600

    Added basic config element to allow more drawing styles

--- HammerBlending.cs
+++ HammerBlending.cs
@@ -6,82 +6,88 @@ using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace HammerBlending
+namespace HammerBlending;
+
+// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+public class HammerBlending : Mod
 {
-  // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-  public class HammerBlending : Mod
-  {
 
+}
+
+// Used for drawing the custom blended tile sprites
+internal class SpecialTile
+{
+  private readonly Tile _tile;
+  private readonly Vector2 _position;
+  private readonly Texture2D _texture;
+  private readonly Color _lightingColor;
+  private readonly SpriteBatch _spriteBatch;
+
+  public SpecialTile(int i, int j, SpriteBatch spriteBatch)
+  {
+    Tile tile = Main.tile[i, j];
+    Vector2 position = new Vector2(i * 16, j * 16) - Main.screenPosition;
+    Texture2D texture = Main.instance.TilesRenderer.GetTileDrawTexture(tile, i, j);
+    Color lightingColor = Lighting.GetColor(i, j);
+
+    _tile = tile;
+    _position = position;
+    _texture = texture;
+    _lightingColor = lightingColor;
+    _spriteBatch = spriteBatch;
   }
 
-  // Used for drawing the custom blended tile sprites
-  internal class SpecialTile
+  public void DrawHalfBlock()
   {
-    private readonly Tile _tile;
-    private readonly Vector2 _position;
-    private readonly Texture2D _texture;
-    private readonly Color _lightingColor;
-    private readonly SpriteBatch _spriteBatch;
+    // Get the bottom half of the full block texture
+    Rectangle halfBlockSource = new Rectangle(
+      _tile.TileFrameX,
+      _tile.TileFrameY + 8,
+      16,
+      8
+    );
 
-    public SpecialTile(int i, int j, SpriteBatch spriteBatch)
+    // Adjust destination to draw bottom half only
+    Rectangle destination = new Rectangle(
+      (int)_position.X,
+      (int)_position.Y + 8,
+      16,
+      8
+    );
+
+    _spriteBatch.Draw(_texture, destination, halfBlockSource, _lightingColor);
+  }
+}
+
+internal class TileBlending : GlobalTile
+{
+  public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch)
+  {
+    Tile tile = Main.tile[i, j];
+
+    bool shouldRegister = tile.Slope switch
     {
-      Tile tile = Main.tile[i, j];
-      Vector2 position = new Vector2(i * 16, j * 16) - Main.screenPosition;
-      Texture2D texture = Main.instance.TilesRenderer.GetTileDrawTexture(tile, i, j);
-      Color lightingColor = Lighting.GetColor(i, j);
+      SlopeType.SlopeDownLeft => Main.tile[i + 1, j].IsHalfBlock,
+      SlopeType.SlopeDownRight => Main.tile[i - 1, j].IsHalfBlock,
+      _ => false
+    };
 
-      _tile = tile;
-      _position = position;
-      _texture = texture;
-      _lightingColor = lightingColor;
-      _spriteBatch = spriteBatch;
-    }
+    if (shouldRegister)
+      Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomSolid);
 
-    public void DrawHalfBlock()
-    {
-      // Get the bottom half of the full block texture
-      Rectangle halfBlockSource = new Rectangle(
-        _tile.TileFrameX,
-        _tile.TileFrameY + 8,
-        16,
-        8
-      );
-
-      // Adjust destination to draw bottom half only
-      Rectangle destination = new Rectangle(
-        (int)_position.X,
-        (int)_position.Y + 8,
-        16,
-        8
-      );
-
-      _spriteBatch.Draw(_texture, destination, halfBlockSource, _lightingColor);
-    }
+    return !shouldRegister;
   }
 
-  internal class TileBlending : GlobalTile
+  public override void SpecialDraw(int i, int j, int type, SpriteBatch spriteBatch)
   {
-    public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch)
+    HammerBlendingConfig config = ModContent.GetInstance<HammerBlendingConfig>();
+    SpecialTile specialTile = new(i, j, spriteBatch);
+
+    switch (config.Variant)
     {
-      Tile tile = Main.tile[i, j];
-
-      bool shouldRegister = tile.Slope switch
-      {
-        SlopeType.SlopeDownLeft => Main.tile[i + 1, j].IsHalfBlock,
-        SlopeType.SlopeDownRight => Main.tile[i - 1, j].IsHalfBlock,
-        _ => false
-      };
-
-      if (shouldRegister)
-        Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomSolid);
-
-      return !shouldRegister;
-    }
-
-    public override void SpecialDraw(int i, int j, int type, SpriteBatch spriteBatch)
-    {
-      SpecialTile specialTile = new(i, j, spriteBatch);
-      specialTile.DrawHalfBlock();
+      case BlendVariant.Basic:
+        specialTile.DrawHalfBlock();
+        break;
     }
   }
 }
---
+++ HammerBlendingConfig.cs
@@ -0,0 +1,19 @@
+using Terraria.ModLoader.Config;
+using System.ComponentModel;
+
+namespace HammerBlending;
+
+public enum BlendVariant
+{
+  Basic,
+  Invisible
+}
+
+public class HammerBlendingConfig : ModConfig
+{
+  public override ConfigScope Mode => ConfigScope.ClientSide;
+
+  [DefaultValue(BlendVariant.Basic)]
+  [Slider]
+  public BlendVariant Variant;
+}
