3caeec282bad02ee7ed173f58e105258d0dfd9e3
Shringe <dashingkoso@gmail.com>
2026-02-12 17:15:19 -0600

    Fixed SpecialDraw trying to blend other mod's tiles

--- src/Hooks.cs
+++ src/Hooks.cs
@@ -8,6 +8,17 @@ namespace HammerBlending;
 
 internal class TileBlending : GlobalTile
 {
+  private bool ShouldBlendTile(int i, int j)
+  {
+    Tile tile = Main.tile[i, j];
+    return tile.Slope switch
+    {
+      SlopeType.SlopeDownLeft => IsHalfBlock(Main.tile[i + 1, j]),
+      SlopeType.SlopeDownRight => IsHalfBlock(Main.tile[i - 1, j]),
+      _ => false
+    };
+  }
+
   private bool IsHalfBlock(Tile tile)
   {
     return tile.IsHalfBlock;
@@ -15,26 +26,22 @@ internal class TileBlending : GlobalTile
 
   public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch)
   {
-    Tile tile = Main.tile[i, j];
-
-    bool shouldRegister = tile.Slope switch
-    {
-      SlopeType.SlopeDownLeft => IsHalfBlock(Main.tile[i + 1, j]),
-      SlopeType.SlopeDownRight => IsHalfBlock(Main.tile[i - 1, j]),
-      _ => false
-    };
-
     // If ModContent.GetInstance has no overhead, it should probably by checked first, but I am assuming it has some overhead here
-    bool shouldBlend = shouldRegister && ModContent.GetInstance<HammerBlendingConfig>().EnableMod;
+    bool shouldBlend = ShouldBlendTile(i, j) && ModContent.GetInstance<HammerBlendingConfig>().EnableMod;
     if (shouldBlend)
       Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomSolid);
-
     return !shouldBlend;
   }
 
   public override void SpecialDraw(int i, int j, int type, SpriteBatch spriteBatch)
   {
+    if (!ShouldBlendTile(i, j))
+      return;
+
     HammerBlendingConfig config = ModContent.GetInstance<HammerBlendingConfig>();
+    if (!config.EnableMod)
+      return;
+
     SpecialTile specialTile = new(i, j, spriteBatch);
 
     switch (config.Variant)
